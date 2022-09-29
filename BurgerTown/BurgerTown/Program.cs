21ff56f4bbdeeb808cb30054be5084e2a75d4d01
Jimmy H. Fortin <2210370@cshawi.ca>
2022-09-28 20:16:30 -0400

    Sqlite implemented

--- BurgerTown/BurgerTown/Program.cs
+++ BurgerTown/BurgerTown/Program.cs
@@ -39,24 +39,7 @@ namespace BurgerTown
             CreateDbIfNotExists(host);
             host.Run();
 
-            // Configure the HTTP request pipeline.
-            /*if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.MapRazorPages();
-
-            app.Run();*/
+            
         }
     }
 }
