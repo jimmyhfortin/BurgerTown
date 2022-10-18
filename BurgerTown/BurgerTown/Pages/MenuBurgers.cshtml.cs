79fc53080d3cb0e5ccceb1bbd80d78b5dc608631
jimmyH <2210370@cshawi.ca>
2022-10-17 23:07:36 -0400

    dev_menu_des_burgers

--- BurgerTown/BurgerTown/Pages/MenuBurgers.cshtml.cs
+++ BurgerTown/BurgerTown/Pages/MenuBurgers.cshtml.cs
@@ -1,12 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BurgerTown.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace BurgerTown.Pages;
 
 public class MenuBurgersModel : PageModel
 {
+    
+    private readonly BurgerTown.Data.DataContext _context;
 
-    public void OnGet()
+    public MenuBurgersModel(BurgerTown.Data.DataContext context)
     {
+        _context = context;
     }
+
+    public IList<Burger> Burger { get;set; } = default!;
+
+    /*public async Task OnGetAsync()
+    {
+        if (_context.Burgers != null)
+        {
+            Burger = await _context.Burgers.ToListAsync();
+        }
+    }*/
+
+    public async Task OnGetAsync()
+    {
+        Burger = await _context.Burgers.ToListAsync();
+    }
+    
 }
